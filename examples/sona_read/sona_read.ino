230abd878c8f904efda95a88e70d14746078bf0d
Narehase <99946234+Narehase@users.noreply.github.com>
2023-01-15 21:18:03 +0900

    Update sona_read.ino

--- examples/sona_read/sona_read.ino
+++ examples/sona_read/sona_read.ino
@@ -1,4 +1,13 @@
+
+/*
+* 
+* 해당 라이브러리는 초음파 센서를 보다 쉽게 사용할수 있도록
+* 만들어졌습니다.  
+*
+*/
+
 #include <sona.h>
+
 
 /*
  * sona mysona(int trig ,int echo ,int type);
@@ -13,7 +22,7 @@
  * 기본값은 1입니다. 
  */
  
-sona mysona(1,2);
+sona mysona(1,2); //sona mysona(int trig ,int echo ,int type);
 
 void setup() {
   Serial.begin(9600);
